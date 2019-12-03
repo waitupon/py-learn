c0095b8f21cf02538f5336fab14d663939153c0f
dingwenhao <dingwenhao@baiwang.com>
2019-12-03 16:52:06 +0800

    cookie

--- com/utils/GetCookie.py
+++ com/utils/GetCookie.py
@@ -1,11 +1,19 @@
-from selenium import webdriver
+#!/bin/env/python
+# *_*coding:utf-8 *_*
+
+from pycookiecheat import chrome_cookies
+import requests
+# import pycookiecheat
 
 
-path = "C:\Program Files (x86)\Google\Chrome\Application"# 注意这个路径需要时可执行路径（chmod 777 dir or 755 dir）
-driver = webdriver.Chrome(executable_path=path)
 
+if __name__ == '__main__':
+    url = 'http://baidu.com/'
 
-#driver = webdriver.Chrome()  # 打开谷歌浏览器,
-driver.get('https://baidu.com')
-cookie = driver.get_cookies()
-driver.quit()
+    # Uses Chrome's default cookies filepath by default
+    cookies = chrome_cookies(url)
+    print(cookies)
+    r = requests.get(url, cookies=cookies)
+    print(r.text)
+
+    pass
